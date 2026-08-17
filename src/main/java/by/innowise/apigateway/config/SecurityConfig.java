d1f3bfba4975c680b06c7ab3d0d6d067ef2559a0
Pavel Kupreichik <91418557+kupreichikpavel@users.noreply.github.com>
2026-08-18 02:21:13 +0300

    feat: implement api gateway integration

---
+++ src/main/java/by/innowise/apigateway/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package by.innowise.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(
+      ServerHttpSecurity http
+  ) {
+    return http
+        .csrf(ServerHttpSecurity.CsrfSpec::disable)
+        .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+        .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+        .authorizeExchange(exchange -> exchange
+            .pathMatchers(
+                HttpMethod.POST,
+                "/api/v1/auth/login",
+                "/api/v1/auth/register"
+            ).permitAll()
+            .pathMatchers("/actuator/health").permitAll()
+            .anyExchange().authenticated()
+        )
+        .oauth2ResourceServer(oauth2 ->
+            oauth2.jwt(Customizer.withDefaults())
+        )
+        .build();
+  }
+}
